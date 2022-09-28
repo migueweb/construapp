ab0bab11653ce79d524933928e46c4c7352a04ce
miang <miguelamadormj@gmail.com>
2022-09-27 21:26:12 -0500

    add help modal

--- calculate_price.php
+++ calculate_price.php
@@ -62,10 +62,17 @@
                 <!-- Start card body-->
                 <div class="card-body mt-2 p-4">
 
-                    <!-- Alert -->
-                    <div class="alert alert-info text-center" role="alert">
+                    <!-- Alert Reminder-->
+                    <div class="alert alert-info alert-dismissible fade show text-center" role="alert">
                         Recuerde colocar las medidas en pulgadas cuadradas <sup>2</sup>
-                    </div>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>                        
+
+                    <!-- Button Help-->
+                    <button type="button" class="btn btn-link p-0 m-0 float-end" data-bs-toggle="modal"
+                        data-bs-target="#exampleModal">
+                        ¿Necesito ayuda?
+                    </button>
 
                     <!-- Start calculate form -->
                     <form action="validate_calculate_price.php" method="post">
@@ -141,4 +148,31 @@
 <div class="m-5 p-3"></div>
 <!-- End fill-->
 
+<!-- Modal Help-->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ayuda</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body bg-light">
+                <p class="fs-5">El formulario actual esta basado en la siguiente configuracion de dos Areas:</p>
+                <img src="./img/image-help-config1.png" alt="configuracion actual de dos paredes" class="img-fluid">
+                
+                <p class="mt-2 fs-5">Donde:</p>
+                <img src="./img/image-help-area1.png" alt="" class="img-fluid">
+                <p class="fs-5">Area 1 Seria el equivalente a pared o muro y </p>
+
+                <img src="./img/image-help-object.png" alt="" class="img-fluid p-2">
+                <p class="fs-5"> Objeto 1 seria el equivalente a un objeto que ocupe espacio en el area (pared o muro) un objeto podria ser una nevera, estufa, lavaplatos, etc...</p>
+                <p class="mt-2 fs-5"> Para realizar una cotizacion debe ingresar el tamaño del area 1 y 2 aparte añadir el tamaño de los objectos si es necesario.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Entendido</button>
+    
+            </div>
+        </div>
+    </div>
+</div>
 <?php include_once './views/footer.php'?>
